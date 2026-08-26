a6cbb6c4da7c487d6ed4a72f9faf7a37503a3e4c
BN-ConsoleArtist <bnestor@consoleartist.com>
2026-08-26 11:02:18 -0700

    WIP PySide GUI implementation

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from colour import (RGB_COLOURSPACES, CCS_COLOURCHECKERS, matrix_colour_correcti
                     xyY_to_XYZ, XYZ_to_RGB, RGB_to_XYZ, XYZ_to_Lab, cctf_encoding, delta_E)
 from PySide6.QtCore import QSize
 from PySide6.QtWidgets import (QPlainTextEdit, QLineEdit, QLabel, QApplication, QMainWindow, QPushButton,
-                               QFileDialog, QVBoxLayout, QHBoxLayout, QBoxLayout, QWidget, QStyle)
+                               QFileDialog, QVBoxLayout, QHBoxLayout, QBoxLayout, QWidget, QStyle, QTableWidget,
+                               QTableWidgetItem, QComboBox)
 
 
 def raw_to_rgb(image_path):
@@ -72,7 +73,6 @@ def calculate_weights_ccm(file_path, validate=False):
     rgb_float32 = file_path
     color_checker_patches = detect_colour_checkers_segmentation(rgb_float32, show=False)
     if not color_checker_patches:
-        print(f'WARNING! failed detection, skipping to next image')
         return None
 
     wb_weights = calculate_wb_weights_llsr(color_checker_patches[0]) #LLSR weights
@@ -80,7 +80,6 @@ def calculate_weights_ccm(file_path, validate=False):
 
     color_checker_reference_values = CCS_COLOURCHECKERS['ColorChecker24 - After November 2014']
     m_t = color_checker_white_balanced
-    print(f'M_T = {m_t}')
     m_r = np.zeros([24,3], dtype=np.float32) # ground truth values
     for index, (name, val) in enumerate(color_checker_reference_values.data.items()):
         xyz_space = xyY_to_XYZ(val)
@@ -107,7 +106,6 @@ def apply_weights_ccm(image_path, weights, ccm):
 
     # Apply White balancing
     rgb_float32 *= weights
-    print(f'Weights = {weights}')
 
     # Apply Color Correction Matrix (CCM)
     rgb_color_corrected = apply_matrix_colour_correction(rgb_float32, ccm, method='Finlayson 2015')
@@ -119,38 +117,39 @@ def apply_weights_ccm(image_path, weights, ccm):
     srgb_uint8 = np.round(np.clip(rgb_gamma_corrected, 0.0, 1.0) * 255).astype(np.uint8)
     return srgb_uint8
 
-def validate_paths(parent_directory):
+def validate_paths(parent_directory, log_callback=None):
     # parent_directory should be a PathLib Path object.
     v_paths = []
     for path in parent_directory.iterdir():
         if path.is_dir():
             source_path = path / 'images_source'.lower()
             color_checker = source_path / 'calibration.cr2'.lower()
-            if color_checker.is_file():
+            if source_path.is_dir() and color_checker.is_file():
                 v_paths.append(path)
             else:
-                print(f'WARNING! No color checker found in {source_path}')
+                log_callback(f"WARNING! No 'images_source' folder or color checker found in {source_path}")
     return sorted(v_paths)
 
-def process_images(image_paths):
+def process_images(image_paths, log_callback=None):
     for path in image_paths:
         source_path = path / 'images_source'
         output_path = path / 'images_calibrated'
         output_path.mkdir(exist_ok=True)
         c_checker_path = source_path/'calibration.cr2'.lower()
-        weights_ccm = calculate_weights_ccm(raw_to_rgb(c_checker_path), validate=True)
+        weights_ccm = calculate_weights_ccm(raw_to_rgb(c_checker_path), validate=False)
         if weights_ccm is None:
+            log_callback(f'WARNING! Patch detection failed on {c_checker_path}, skipping to next folder')
             continue
         w = weights_ccm[0]
         ccm = weights_ccm[1]
 
         for f in source_path.iterdir():
             if f.suffix.lower() == ".cr2":
-                print(f'Processing {f}')
+                log_callback(f'Processing {f}')
                 file_path = Path(output_path / f.name).with_suffix('.tga')
                 processed_file = apply_weights_ccm(f, w, ccm)
                 if processed_file is not None:
-                    print(f'Saving {file_path}')
+                    log_callback(f'Saving {file_path}')
                     iio.imwrite(str(file_path), processed_file)
 
 def validate_color_correction(ccm, target, reference):
@@ -170,7 +169,156 @@ def validate_color_correction(ccm, target, reference):
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.parent_folder = "Choose Folder..."
+        self.valid_paths = []
+        self.export_format = ['.tga', '.png', '.tif', '.jpg', '.bmp']
+        self.Dropdown_label = QLabel('Choose Export Format')
 
+        # Set the window title and initial size
+        self.setWindowTitle("Color Checker Batch Color Correction")
+        self.resize(1200, 600)
+        self.setStyleSheet("background-color: lightgrey;")
+
+        # Create layout and attributes
+        VLayout_main = QVBoxLayout()
+        VLayout_main.setDirection(QBoxLayout.Direction.TopToBottom)
+        VLayout_main.setSpacing(10)
+
+        HLayout1 = QHBoxLayout()
+        HLayout1.setDirection(QBoxLayout.Direction.LeftToRight)
+
+        HLayout2 = QHBoxLayout()
+        HLayout2.setDirection(QBoxLayout.Direction.LeftToRight)
+
+        VLayout1 = QVBoxLayout()
+        VLayout1.setDirection(QBoxLayout.Direction.TopToBottom)
+
+
+        # Create button widgets
+        self.folder_button = QPushButton()
+        self.folder_button.setFixedSize(32, 32)
+        self.folder_button.setStyleSheet("background-color: transparent; border: none;")
+        folder_button_style = self.folder_button.style()
+        folder_icon = folder_button_style.standardIcon(QStyle.StandardPixmap.SP_DirIcon)
+        self.folder_button.setIcon(folder_icon)
+        self.folder_button.setIconSize(QSize(32, 32))
+        self.folder_button.clicked.connect(self.get_parent_folder)
+
+
+        self.search_button = QPushButton("Scan Parent Folder")
+        self.search_button.setEnabled(False) #Keep disabled until initial file selected
+        self.search_button.setFixedSize(200,50)
+        self.search_button.clicked.connect(self.scan_folders)
+
+        self.process_button = QPushButton("Process Images")
+        self.process_button.setEnabled(False) #Keep disabled until valid directories found
+        self.process_button.setFixedSize(200,50)
+        self.process_button.clicked.connect(self.process_images)
+
+
+
+        # Create file path text widgets
+        self.display_parent_folder = QLineEdit(self.parent_folder)
+        self.display_parent_folder.setReadOnly(True)
+        self.display_parent_folder.setStyleSheet("background-color: white; border: none;")
+
+        # Create Label Widgets
+        self.label1 = QLabel("Parent Folder: ", self)
+        self.label2 = QLabel("Search Directory: ", self)
+        self.Dropdown_label = QLabel('Choose Export Format:')
+
+
+        # #Creat Table Widget
+        # self.table = QTableWidget()
+        # self.table_rows = 10
+        # self.table_columns = 3
+        # self.table.setColumnCount(self.table_columns)
+        # self.table.setRowCount(self.table_rows)
+        # self.table.setStyleSheet("background-color: white; border: none;")
+        # self.table.setItem(2, 1, QTableWidgetItem("Parent Folder: "))
+
+        #Create a Combo Box (drop down menu) Widget
+
+        self.dropdown = QComboBox()
+        self.dropdown.setFixedSize(200, 25)
+        # self.dropdown.setStyleSheet("background-color: white;")
+        self.dropdown.insertItems(0, self.export_format)
+
+        # Create Terminal Widget
+        self.terminal = QPlainTextEdit()
+        self.terminal.setReadOnly(True)
+        self.terminal.setMaximumBlockCount(100)
+        self.terminal.appendPlainText('Log Updates...')
+        self.terminal.setMinimumHeight(250)
+        self.terminal.setStyleSheet("background-color: white;")
+
+
+
+        # Assemble the layouts, add widgets, nest HLayouts under VLayout_main
+        HLayout1.addWidget(self.label1)
+        HLayout1.addWidget(self.display_parent_folder)
+        HLayout1.addWidget(self.folder_button)
+
+        VLayout1.setSpacing(25)
+        VLayout1.addWidget(self.search_button)
+        VLayout1.addWidget(self.process_button)
+        VLayout1.addWidget(self.Dropdown_label)
+        VLayout1.addWidget(self.dropdown)
+        VLayout1.addStretch()
+
+
+
+        HLayout2.addLayout(VLayout1)
+        HLayout2.addWidget(self.terminal)
+
+
+        # Main assembly
+        VLayout_main.addLayout(HLayout1)
+        VLayout_main.addLayout(HLayout2)
+
+
+      # Create a central widget, set the layout, and apply it to the QMainWindow
+        central_widget = QWidget()
+        central_widget.setLayout(VLayout_main)
+        self.setCentralWidget(central_widget)
+
+    def get_parent_folder(self):
+        selected_dir = QFileDialog.getExistingDirectory(
+            parent=self,
+            caption="Select Directory",
+            dir="",  # Starting directory (empty string defaults to the current working directory)
+            options=QFileDialog.Option.ShowDirsOnly  # Restricts selection to directories only
+        )
+        if not selected_dir:
+            self.terminal.appendPlainText('Selection cancelled')
+            return
+        self.parent_folder = Path(selected_dir)
+        self.display_parent_folder.setText(str(self.parent_folder))
+        self.terminal.appendPlainText(f'Parent folder: {str(self.parent_folder)}')
+        self.search_button.setEnabled(True)
+
+    def scan_folders(self):
+        self.search_button.setEnabled(False)
+        self.terminal.appendPlainText('\nScanning for valid subfolders...')
+        self.valid_paths = validate_paths(self.parent_folder, self.terminal.appendPlainText)
+        if len(self.valid_paths) == 0:
+            self.terminal.appendPlainText('No valid subfolders found')
+            self.process_button.setEnabled(False)
+        else:
+            self.terminal.appendPlainText('\nValid paths found:')
+            self.process_button.setEnabled(True)
+            for v in self.valid_paths:
+                self.terminal.appendPlainText(str(v))
+
+        self.search_button.setEnabled(True)
+
+    def process_images(self):
+        print("Background task started...")
+        self.search_button.setEnabled(False)
+        self.terminal.appendPlainText('\nProcessing images...')
+        process_images(self.valid_paths, self.terminal.appendPlainText)
+        self.search_button.setEnabled(True)
+        print("Background task finished!")
 
 
 if __name__ == "__main__":
@@ -178,9 +326,19 @@ if __name__ == "__main__":
     np.set_printoptions(suppress=True, precision=6)
     p_directory = Path('/Users/renderman/Documents/Python/Image_processing/Photogrammetry')
 
-    valid_paths = validate_paths(p_directory)
+    # Create the application instance
+    app = QApplication(sys.argv)
 
-    process_images(valid_paths)
+    # Create and show the window
+    window = Window()
+    window.show()
+
+    # Start the event loop
+    sys.exit(app.exec())
+
+    # valid_paths = validate_paths(p_directory)
+    #
+    # process_images(valid_paths)
 
 
 
